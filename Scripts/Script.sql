bfa6b970fcfe6c7cf7fa5e1f4680e0d8bdbc62ea
hyokyun <gyrbs22@gmail.com>
2021-05-09 18:49:52 +0900

    Update Script.sql
    DB CRUD - 1 (2021.05.09)

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -1,2 +1,18 @@
-create database school;
-show databases;
+create table student(
+	number int,
+	name varchar(30),
+	age int,
+	class int,
+	height float
+);
+
+insert into student(number, name, age, class, height)
+values(1, "½ºÅºÀÌ", 17, 1, 173.3)
+
+insert into student(number, name, age, height)
+values(2, "¸£ÅºÀÌ", 17, 173.3)
+
+
+select number, name, age, class, height from student;
+
+select * from student where class is null;
