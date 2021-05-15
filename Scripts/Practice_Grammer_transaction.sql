731040ae32de270883960dd9252dd963b65c9937
hyokyun <gyrbs22@gmail.com>
2021-05-15 19:41:11 +0900

    Create Practice_Grammer_transaction.sql
    tansaction

---
+++ Scripts/Practice_Grammer_transaction.sql
@@ -0,0 +1,9 @@
+set autocommit = 0;
+
+start transaction;
+	select * from student;
+	update student set name = "서빈2" where id = 8;
+	/*이 사이에 COMMIT을 하면 서빈2로 UPDATE한 내역은 저장된다*/
+	delete from student where id = 9;
+	select * from student;
+rollback;
